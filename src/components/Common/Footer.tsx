b5ce693c1f71ea52bd32cbcd29c6c92d2d5c3efb
Zakariya Qureshi <zakariyaq313@gmail.com>
2022-06-04 20:18:37 +0530

    Fix broken link

--- src/components/Common/Footer.tsx
+++ src/components/Common/Footer.tsx
@@ -2,7 +2,7 @@ function Footer(): JSX.Element {
 	return (
 		<footer>
 			<p>
-				Created by <a className="rising-background" href="https://zakariyaq313.github.io/my_website/" target="_blank" rel="noreferrer noopener">Muhammad Zakariya</a>
+				Created by <a className="rising-background" href="https://zakariyaq313.github.io/my-website/" target="_blank" rel="noreferrer noopener">Muhammad Zakariya</a>
 			</p>
 		</footer>
 	);
